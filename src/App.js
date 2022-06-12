3324c08e80cc4616e7285b5c58ed7b0c7c8bb774
ptklemm <phillipklemm@gmail.com>
2022-06-11 18:41:41 -0700

    API retrieval working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import "./App.css";
 
-console.log(process.env.REACT_APP_WEATHERAPI_KEY);
+const WEATHERAPI_URL_BASE = "https://api.weatherapi.com/v1";
+const WEATHERAPI_URL_CURRENT = WEATHERAPI_URL_BASE + "/current.json";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [geolocation, setGeolocation] = useState(null);
+	const [currentWeather, setCurrentWeather] = useState(0);
 
 	useEffect(() => {
-		console.log(`Button has been incremented ${count} times`);
-	});
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					console.log(position);
+					setGeolocation(position);
+				},
+				(error) => {
+					console.error("Error retrieving geolocation: " + error.code + " - " + error.message);
+				}
+			);
+		} else {
+			console.warn("Geolocation API not found.");
+		}
+	}, []);
+
+	useEffect(() => {
+		if (geolocation) {
+			fetch(
+				`${WEATHERAPI_URL_CURRENT}?key=${process.env.REACT_APP_WEATHERAPI_KEY}&q=${geolocation.coords.latitude},${geolocation.coords.longitude}&aqi=yes`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+					setCurrentWeather(data);
+				});
+		}
+	}, [geolocation]);
 
 	return (
-		<div className="App">
-			<p>Test React Hooks</p>
-			<button onClick={() => setCount(count + 1)}>Increment</button>
-			<p>{count}</p>
+		<div className="vertical-centered-container">
+			<Card>
+				<Card.Body>
+					{currentWeather.location.name} {currentWeather.current.temp_f}
+				</Card.Body>
+			</Card>
 		</div>
 	);
 }
