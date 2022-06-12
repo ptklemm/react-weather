09dac7da97381b34e201321cf9ad261d24511a17
ptklemm <phillipklemm@gmail.com>
2022-06-11 23:15:26 -0700

    Remove direct call to WeatherAPI

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,11 +5,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import "./App.css";
 
-const WEATHERAPI_URL_BASE = "https://api.weatherapi.com/v1";
-const WEATHERAPI_URL_CURRENT = WEATHERAPI_URL_BASE + "/current.json";
-
 function App() {
-	const [geolocation, setGeolocation] = useState(null);
 	const [currentWeather, setCurrentWeather] = useState(null);
 
 	useEffect(() => {
@@ -17,7 +13,6 @@ function App() {
 			navigator.geolocation.getCurrentPosition(
 				(position) => {
 					console.log(position);
-					setGeolocation(position);
 				},
 				(error) => {
 					console.error("Error retrieving geolocation: " + error.code + " - " + error.message);
@@ -27,19 +22,6 @@ function App() {
 			console.warn("Geolocation API not found.");
 		}
 	}, []);
-
-	useEffect(() => {
-		if (geolocation) {
-			fetch(
-				`${WEATHERAPI_URL_CURRENT}?key=${process.env.REACT_APP_WEATHERAPI_KEY}&q=${geolocation.coords.latitude},${geolocation.coords.longitude}&aqi=yes`
-			)
-				.then((response) => response.json())
-				.then((data) => {
-					console.log(data);
-					setCurrentWeather(data);
-				});
-		}
-	}, [geolocation]);
 
 	return (
 		<div className="vertical-centered-container">
